04881be9d4d898cfce54177fd36bcfc30f012804
Naruto777549649 <sikrar026@gmail.com>
2025-04-21 07:52:25 +0530

    Update config.py

--- MakimaRobot/config.py
+++ MakimaRobot/config.py
@@ -15,17 +15,17 @@ def get_user_list(config, key):
 class Config(object):
     LOGGER = True
 
-    API_ID = "1480988"
-    API_HASH = "be76b2fd25b50222b0e1eee141d6a259"
-    TOKEN = "5605728947:AAHGZRsBITZF6LY8L4_08pGwvdcMkKpXrCE"
-    OWNER_ID = "5544822530"
-    OWNER_USERNAME = "FULLY_CRUAL_MIND"
-    SUPPORT_CHAT = "KanimangalamKovilakam"
-    JOIN_LOGGER = (-1001767332965)
-    EVENT_LOGS = (-1001767332965)
+    API_ID = "25698862"
+    API_HASH = "7d7739b44f5f8c825d48cc6787889dbc"
+    TOKEN = "7502712674:AAGi4UOsab4oY7iQ6qFyZZmm4BizOT2MfIs"
+    OWNER_ID = "7019600964"
+    OWNER_USERNAME = "Uzumaki_X_Naruto_6"
+    SUPPORT_CHAT = "Animeheaven_community"
+    JOIN_LOGGER = (-1002515671821)
+    EVENT_LOGS = (-1002535643821)
 
     SQLALCHEMY_DATABASE_URI = ""
-    MONGO_DB_URI = "mongodb+srv://Stenzlemaria:Stenzlemaria@cluster0.0ob4dvz.mongodb.net/?retryWrites=true&w=majority"
+    MONGO_DB_URI = "mongodb+srv://Naruto:Naruto777549@cluster1.ge8jrgm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1"
     LOAD = []
     NO_LOAD = ["rss"]
     WEBHOOK = False
